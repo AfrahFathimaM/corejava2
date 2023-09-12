d880b3fcede9b80b9c0c5d35730fba3f121084fb
AfrahFathimaM <safrah154@gmail.com>
2023-09-12 20:06:27 +0530

    static

---
+++ c2tc_c21/src/com/tns/Interfaceprog/Mainclass.java
@@ -0,0 +1,5 @@
+package com.tns.Interfaceprog;
+
+public class Mainclass {
+
+}
---
+++ c2tc_c21/src/staticc/staticcDemo.java
@@ -0,0 +1,13 @@
+package staticc;
+
+public class staticcDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(employee.companyName);
+		employee ob = new employee(" Reshma ", 123);
+		System.out.println(ob);
+		employee ob1 = new employee(" Kishore ", 785);
+		System.out.println(ob1);
+	}
+}
